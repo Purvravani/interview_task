47256837528930c29aba95df015de7f19d89a291
Purvravani <133305149+Purvravani@users.noreply.github.com>
2023-06-10 19:08:08 +0530

    final

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+
 class demo {
-  Future<void> get_data()
-  async {
+  Future<void> get_data() async {
 // Get a location using getDatabasesPath
     var databasesPath = await getDatabasesPath();
     String path = join(databasesPath, 'purv.db');
@@ -14,9 +14,10 @@ class demo {
 // open the database
     Database database = await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
-          // When creating the db, create the table
-          await db.execute(
-              'CREATE TABLE contact_book (id INTEGER PRIMARY KEY, name TEXT, contact TEXT, )');
-        });
+      // When creating the db, create the table
+      await db.execute(
+          'CREATE TABLE contact_book (id INTEGER PRIMARY KEY, name TEXT, contact TEXT,DOB TEXT,pass TEXT,cpss TEXT )');
+    });
   }
+
 }
